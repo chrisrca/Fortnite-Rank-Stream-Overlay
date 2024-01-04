c3b09da4b73fd61f4d6e83673d572d154b45c99d
chrisrca <goalie.cyclone@gmail.com>
2024-01-04 00:37:01 -0500

    modified bars to match rank and new name input

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const readline = require('readline');
+
 const base = 'http://160.251.9.161:8080';
 const fontPath = path.join(__dirname, 'font.ttf');
 const icons = [
@@ -12,17 +14,13 @@ const icons = [
     'diamond_01.png', 'diamond_02.png', 'diamond_03.png',
     'Elite.png', 'Champion.png', 'Unreal.png'
 ];
-const names = [
-    'BRONZE I', 'BRONZE II', 'BRONZE III',
-    'SILVER I', 'SILVER II', 'SILVER III',
-    'GOLD I', 'GOLD II', 'GOLD III',
-    'PLATINUM I', 'PLATINUM II', 'PLATINUM III',
-    'DIAMOND I', 'DIAMOND II', 'DIAMOND III',
-    'ELITE', 'CHAMPION', 'UNREAL', 'UNRANKED'
-];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Replace these with actual values or ways to get these values in Node.js
-const account_id = '1253253834fa4df9b017c99e25a32f8c';
+let account_id = '';
 const ranking_type = 'ranked-br';
 const port = 3000;
 let percentage = 100;
@@ -47,8 +45,8 @@ async function update() {
             index = Math.floor(entry.currentDivision / 3);
         }
 
-        const rankIcon = `src/${icons[entry.currentDivision]}`;
-        const rankName = names[entry.currentDivision];
+        const rankIcon = `src/icons/${icons[entry.currentDivision]}`;
+        let rankBar = (`src/bars/${icons[entry.currentDivision]}`).replace(/(_01|_02|_03|.png)/g, ``)
         let progress;
 
         if (entry.currentPlayerRanking !== null) {
@@ -57,7 +55,7 @@ async function update() {
             const value = Math.round(entry.promotionProgress * 100);
             progress = value;
         }
-        return {rankIcon, rankName, progress}
+        return {rankIcon, rankBar, progress}
       } catch (error) {
         console.error("Error occurred:", error);
         return { rankIcon: null, rankName: null, progress: null };
@@ -72,99 +70,351 @@ const server = http.createServer(async (req, res) => { // Make this callback asy
       res.setHeader('Refresh', '25'); 
       res.statusCode = 200;
 
-      const rank = await update(); // Use await to wait for the promise
-      console.log(rank.rankIcon);
-      console.log(rank.progress);
+      const rank = await update(); 
+      // console.log(rank.rankIcon);
+      // console.log(rank.progress);
+      // console.log(rank.rankBar);
+
       percentage = rank.progress
       icon = rank.rankIcon
 
-    if (percentage < 2.4) {
-      percentage = 2.4;
-    }
-    convertedpercentage = percentage * 3.33;
-    const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <style>
-        @font-face { 
-            font-family: "Burbank Big Condensed Bold"; 
-            src: url('/font.ttf'); 
-        }
-        .container {
-            background-size: cover;
+    let htmlContent = ``
+    let convertedpercentage = percentage * 3.33;
+    if (percentage == 0 && icon === "src/icons/bronze_01.png") { // Unranked
+      htmlContent = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <style>
+          @font-face { 
+              font-family: "Burbank Big Condensed Bold"; 
+              src: url('/font.ttf'); 
+          }
+          .container {
+              background-size: cover;
+              position: relative;
+              text-align: center;
+              color: rgb(255, 255, 255);
+          }
+          .text {
+              position: absolute;
+              font-size: 19px;
+              left: 399px;
+              top: 9px;
+              font-family: "Burbank Big Condensed Bold", sans-serif;
+              transform: scaleX(0.80);
+              letter-spacing: 1px;
+          }
+          .image-container {
             position: relative;
-            text-align: center;
-            color: rgb(255, 255, 255);
+            left: 50px
         }
-        .text {
-            position: absolute;
-            font-size: 19px;
-            left: 399px;
-            top: 9px;
-            font-family: "Burbank Big Condensed Bold", sans-serif;
-            transform: scaleX(0.80);
-            letter-spacing: 1px;
+        .precent {
+            position: relative;
+            left: 50px
         }
-        .image-container {
-          position: relative;
-          left: 50px
-      }
-      .precent {
-          position: relative;
-          left: 50px
-      }
-        .image-rank {
-            position: absolute;
-            top: -1px;
-            left: 20px;
-            z-index: 3;
-            width: 44px;  
-            height: 44px;
+          .image-rank {
+              position: absolute;
+              top: -1px;
+              left: 20px;
+              z-index: 3;
+              width: 44px;  
+              height: 44px;
+          }
+          .image-percent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 1;
+              width: 352px;
+              height: 43px;
+              clip-path: polygon(0 0, 342px 0, 342px 100%, 0 100%); /* Crop the width */
+          }
+  
+          .image-transparent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 0;
+          }
+  
+          .image-current {
+              position: absolute;
+              top: 0;
+              left: 0px;
+              z-index: 3;
+          }
+      </style>
+  </head>
+      <body>
+      <div>
+      <div class="rank">
+          <img class="image-rank" src="src/icons/Unranked.png">
+      </div>
+      
+      <div class="image-container">
+          <img class="image-current" src="src/bars/Unranked_current.png">
+          <img class="image-percent" src="src/bars/Unranked_percentage.png">
+          <img class="image-transparent" src="src/transparent.png">
+      </div>
+      <div class="percent">
+          <div class="container"><div class="text"></div>
+      </div>
+  </div>
+  </body>
+  </html>
+      `;
+    } else if (percentage < 2.4) { // Prevent Bar from rendering incorrectly under 2.4%
+      htmlContent = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <style>
+          @font-face { 
+              font-family: "Burbank Big Condensed Bold"; 
+              src: url('/font.ttf'); 
+          }
+          .container {
+              background-size: cover;
+              position: relative;
+              text-align: center;
+              color: rgb(255, 255, 255);
+          }
+          .text {
+              position: absolute;
+              font-size: 19px;
+              left: 399px;
+              top: 9px;
+              font-family: "Burbank Big Condensed Bold", sans-serif;
+              transform: scaleX(0.80);
+              letter-spacing: 1px;
+          }
+          .image-container {
+            position: relative;
+            left: 50px
         }
-        .image-percent {
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: 1;
-            width: 352px;
-            height: 43px;
-            clip-path: polygon(0 0, ${9 + convertedpercentage}px 0, ${9 + convertedpercentage}px 100%, 0 100%); /* Crop the width */
+        .precent {
+            position: relative;
+            left: 50px
         }
-
-        .image-transparent {
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: 0;
+          .image-rank {
+              position: absolute;
+              top: -1px;
+              left: 20px;
+              z-index: 3;
+              width: 44px;  
+              height: 44px;
+          }
+          .image-percent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 1;
+              width: 352px;
+              height: 43px;
+              clip-path: polygon(0 0, ${9 + convertedpercentage}px 0, ${9 + convertedpercentage}px 100%, 0 100%); /* Crop the width */
+          }
+  
+          .image-transparent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 0;
+          }
+  
+          .image-current {
+              position: absolute;
+              top: 0;
+              left: ${0 - (342 - convertedpercentage - 9)}px;
+              z-index: 3;
+          }
+      </style>
+  </head>
+      <body>
+      <div>
+      <div class="rank">
+          <img class="image-rank" src="${icon}">
+      </div>
+      
+      <div class="image-container">
+          <img class="image-transparent" src="src/transparent.png">
+      </div>
+      <div class="percent">
+          <div class="container"><div class="text">${percentage + '%'}</div>
+      </div>
+  </div>
+  </body>
+  </html>
+      `;
+    } else if (icon === "src/icons/Unreal.png") { // Unreal
+      htmlContent = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <style>
+          @font-face { 
+              font-family: "Burbank Big Condensed Bold"; 
+              src: url('/font.ttf'); 
+          }
+          .container {
+              background-size: cover;
+              position: relative;
+              text-align: center;
+              color: rgb(255, 255, 255);
+          }
+          .text {
+              position: absolute;
+              font-size: 19px;
+              left: 399px;
+              top: 9px;
+              font-family: "Burbank Big Condensed Bold", sans-serif;
+              transform: scaleX(0.80);
+              letter-spacing: 1px;
+          }
+          .image-container {
+            position: relative;
+            left: 50px
         }
-
-        .image-current {
-            position: absolute;
-            top: 0;
-            left: ${0 - (342 - convertedpercentage - 9)}px;
-            z-index: 3;
+        .precent {
+            position: relative;
+            left: 50px
         }
-    </style>
-</head>
-    <body>
-    <div>
-    <div class="rank">
-        <img class="image-rank" src="${icon}">
-    </div>
-    
-    <div class="image-container">
-        <img class="image-current" src="src/current.png">
-        <img class="image-percent" src="src/percentage.png">
-        <img class="image-transparent" src="src/transparent.png">
-    </div>
-    <div class="percent">
-        <div class="container"><div class="text">${percentage + '%'}</div>
-    </div>
-</div>
-</body>
-</html>
-    `;
+          .image-rank {
+              position: absolute;
+              top: -1px;
+              left: 20px;
+              z-index: 3;
+              width: 44px;  
+              height: 44px;
+          }
+          .image-percent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 1;
+              width: 352px;
+              height: 43px;
+              clip-path: polygon(0 0, ${9 + convertedpercentage}px 0, ${9 + convertedpercentage}px 100%, 0 100%); /* Crop the width */
+          }
+  
+          .image-transparent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 0;
+          }
+  
+          .image-current {
+              position: absolute;
+              top: 0;
+              left: ${0 - (342 - convertedpercentage - 9)}px;
+              z-index: 3;
+          }
+      </style>
+  </head>
+      <body>
+      <div>
+      <div class="rank">
+          <img class="image-rank" src="${icon}">
+      </div>
+      
+      <div class="image-container">
+          <img class="image-current" src="${rank.rankBar}_current.png">
+          <img class="image-percent" src="${rank.rankBar}_percentage.png">
+          <img class="image-transparent" src="src/transparent.png">
+      </div>
+      <div class="percent">
+          <div class="container"><div class="text">${percentage}</div>
+      </div>
+  </div>
+  </body>
+  </html>
+      `;
+    } else { // Render Rank and Bar
+      htmlContent = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <style>
+          @font-face { 
+              font-family: "Burbank Big Condensed Bold"; 
+              src: url('/font.ttf'); 
+          }
+          .container {
+              background-size: cover;
+              position: relative;
+              text-align: center;
+              color: rgb(255, 255, 255);
+          }
+          .text {
+              position: absolute;
+              font-size: 19px;
+              left: 399px;
+              top: 9px;
+              font-family: "Burbank Big Condensed Bold", sans-serif;
+              transform: scaleX(0.80);
+              letter-spacing: 1px;
+          }
+          .image-container {
+            position: relative;
+            left: 50px
+        }
+        .precent {
+            position: relative;
+            left: 50px
+        }
+          .image-rank {
+              position: absolute;
+              top: -1px;
+              left: 20px;
+              z-index: 3;
+              width: 44px;  
+              height: 44px;
+          }
+          .image-percent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 1;
+              width: 352px;
+              height: 43px;
+              clip-path: polygon(0 0, ${9 + convertedpercentage}px 0, ${9 + convertedpercentage}px 100%, 0 100%); /* Crop the width */
+          }
+  
+          .image-transparent {
+              position: absolute;
+              top: 0;
+              left: 0;
+              z-index: 0;
+          }
+  
+          .image-current {
+              position: absolute;
+              top: 0;
+              left: ${0 - (342 - convertedpercentage - 9)}px;
+              z-index: 3;
+          }
+      </style>
+  </head>
+      <body>
+      <div>
+      <div class="rank">
+          <img class="image-rank" src="${icon}">
+      </div>
+      
+      <div class="image-container">
+          <img class="image-current" src="${rank.rankBar}_current.png">
+          <img class="image-percent" src="${rank.rankBar}_percentage.png">
+          <img class="image-transparent" src="src/transparent.png">
+      </div>
+      <div class="percent">
+          <div class="container"><div class="text">${percentage + '%'}</div>
+      </div>
+  </div>
+  </body>
+  </html>
+      `;
+    }
 
     res.end(htmlContent);
   } else if (req.url === '/font.ttf') {
@@ -199,6 +449,23 @@ const server = http.createServer(async (req, res) => { // Make this callback asy
   }
 });
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+rl.question('Please enter your display name: ', function (userName) {
+  if (userName) {
+      const formattedName = userName.replace(/\s/g, '%20');
+      const url = `http://160.251.9.161:8080/api/fortnite/rank/fetch_user_by_display_name?display_name=${formattedName}`;
+      axios.get(url)
+          .then(response => {
+            account_id = response.data.account_id; // Corrected to access data property
+            console.log("Account ID:", account_id);
+            server.listen(port, () => {
+              console.log(`Server running on http://localhost:${port}/`);
+            });
+          })
+          .catch(error => {
+              console.error('There has been a problem with your HTTP request:', error);
+          });
+  } else {
+      console.log("No name was entered.");
+  }
+  rl.close();
 });
